2ae8c700bc450aefc16a90e51212b3301bd0285d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-08 04:09:00 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,11 +6,18 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm \
+	fluidsynth      \
+	gvfs            \
+	lutris          \
+	python-pefile   \
+	python-protobuf \
+	wine            \
+	winetricks
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common
 
 # Comment this out if you need an AUR package
 #make-aur-package PACKAGENAME
